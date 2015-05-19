66646c74866650c36cab2910cca47b0784adff8c
Kamran Shahzad <bleak.unseen@gmail.com>
2015-05-19 20:24:14 +0500

    initial projects added

---
+++ assets/scripts/static-data.js
@@ -0,0 +1,428 @@
+
+
+
+
+var projects = [
+    {
+        title:'Viewport',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-1',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Suggest List',
+        icon:'fa fa-list',
+        bgclass:'b-color-2',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'TagsInput',
+        icon:'fa fa-tag',
+        bgclass:'b-color-3',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Single Line Form',
+        icon:'fa fa-square-o',
+        bgclass:'b-color-4',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Bulk Upload',
+        icon:'fa fa-upload',
+        bgclass:'b-color-5',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Responsive Menus',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-6',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Layout Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-7',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Theme Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-8',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'XMLForm Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-9',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Dashboard Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-10',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Taxonomy Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-11',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'GroupList Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-12',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'CodeSnippet Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-13',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'CodeMirror Bundle',
+        icon:'fa fa-mobile',
+        bgclass:'b-color-14',
+        description: 'A modern dialog library which is highly configurable and easy to style.' ,
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+];
+
+
+
+
+var inline_projects = [
+    {
+        title:'Contacts Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'AdsBuilder Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Activity Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Article Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Data Mining',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Python',
+        color: 'color-2',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Link Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'PlusAssetic Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Core Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Settings Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'MediaTool Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'CSS Framework',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'CSS3',
+        color: 'color-3',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'User Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Permissions Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'BaseJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Help Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Report Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'BaseUtilJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'DomJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'AnimateJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'WidgetJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Front Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Organization Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'PhpDoc Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'FormJS',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Javascript',
+        color: 'color-4',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Util Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Migration Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'EmailBuilder Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'Email Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+    {
+        title:'AngularForm Bundle',
+        url: '#',
+        description:'It is a long established fact that a reader will be distracted by the',
+        technology: 'Symfony2',
+        color: 'color-1',
+        links:[
+            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
+        ]
+    },
+];
