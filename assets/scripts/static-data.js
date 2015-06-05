6ed9c157764f707805d05cecbf17b88b94baceb2
Kamran Shahzad <bleak.unseen@gmail.com>
2015-06-05 19:14:35 +0500

    data added

--- assets/scripts/static-data.js
+++ assets/scripts/static-data.js
@@ -43,11 +43,11 @@ var header_data = {
 var footer_links = {
     links:[
         {label:"",url:"https://twitter.com/kamran_shahzed",icon:"fa fa-twitter"},
-        {label:"",url:"",icon:"fa fa-facebook"},
+        {label:"",url:"https://www.facebook.com/profile.php?id=100008768106919&fref=ts",icon:"fa fa-facebook"},
         {label:"",url:"https://pk.linkedin.com/pub/kamran-shahzad/98/a16/47",icon:"fa fa-linkedin"},
         {label:"",url:"https://plus.google.com/+KamranShahzads",icon:"fa fa-google-plus"},
         {label:"",url:"",icon:"fa fa-stack-overflow"},
-        {label:"",url:"",icon:"fa fa-codepen"},
+        {label:"",url:"http://codepen.io/kamranshahzad/",icon:"fa fa-codepen"},
         {label:"",url:"https://github.com/kamranshahzad",icon:"fa fa-git"},
     ]
 };
@@ -93,26 +93,6 @@ var article_list = {
 */
 var projects = [
     {
-        title:'Viewport',
-        icon:'fa fa-mobile',
-        bgclass:'b-color-1',
-        url: "#",
-        description: "Multiple views to check responsive layouts on different dimensions." ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'npmjs',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Suggest List',
-        icon:'fa fa-list',
-        bgclass:'b-color-2',
-        url: "#",
-        description: 'Suggest list for input field using VanillaJS' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'npmjs',url:'http://knd/domjs'}
-        ]
-    },
-    {
         title:'TagsInput',
         icon:'fa fa-tag',
         bgclass:'b-color-3',
@@ -132,268 +112,20 @@ var projects = [
             {label:'github',url:'http://www.domjs.com'},{label:'npmjs',url:'http://knd/domjs'}
         ]
     },
-    {
-        title:'Bulk Upload',
-        icon:'fa fa-upload',
-        bgclass:'b-color-5',
-        url: "#",
-        description: 'Import data from ms excel, assign custom columns.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Responsive Menus',
-        icon:'fa fa-cube',
-        bgclass:'b-color-6',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Layout Bundle',
-        icon:'fa fa-desktop',
-        bgclass:'b-color-7',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Theme Bundle',
-        icon:'fa fa-laptop',
-        bgclass:'b-color-8',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'XMLForm Bundle',
-        icon:'fa fa-code',
-        bgclass:'b-color-9',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Dashboard Bundle',
-        icon:'fa fa-building',
-        bgclass:'b-color-10',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Taxonomy Bundle',
-        icon:'fa fa-tree',
-        bgclass:'b-color-11',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'GroupList Bundle',
-        icon:'fa fa-sort-amount-asc',
-        bgclass:'b-color-12',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'CodeSnippet Bundle',
-        icon:'fa fa-file-code-o',
-        bgclass:'b-color-13',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'CodeMirror Bundle',
-        icon:'fa fa-pencil-square-o',
-        bgclass:'b-color-14',
-        url: "#",
-        description: 'A modern dialog library which is highly configurable and easy to style.' ,
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
+
 ];
 
 
 
 
 var inline_projects = [
-    {
-        title:'Contacts Bundle',
-        url: '#',
-        description:"This bundle store organization's employee contacts like directory.",
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'AdsBuilder Bundle',
-        url: '#',
-        description:'You can easily create ads and ads block in theme pages',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Activity Bundle',
-        url: '#',
-        description:'It is a long established fact that a reader will be distracted by the',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Article Bundle',
-        url: '#',
-        description:'Article manager provides all step for article writing.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Data Mining',
-        url: '#',
-        description:'Provide interactive interface for view and matching data w.r.t to criteria.',
-        technology: 'Python',
-        color: 'color-2',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Link Bundle',
-        url: '#',
-        description:'Manage links with nested categories.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'PlusAssetic Bundle',
-        url: '#',
-        description:'Provide extra futures for asset management for Symfony2',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Core Bundle',
-        url: '#',
-        description:'This bundle provide a wrapper for fast development.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Settings Bundle',
-        url: '#',
-        description:'This bundle provides settings form for all bundles.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'MediaTool Bundle',
-        url: '#',
-        description:'It is a long established fact that a reader will be distracted by the',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'CSS Framework',
-        url: '#',
-        description:'CSS helper library to build interactive interfaces.',
-        technology: 'CSS3',
-        color: 'color-3',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'User Bundle',
-        url: '#',
-        description:'User management bundle for Symfony2',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Permissions Bundle',
-        url: '#',
-        description:'A bundle for Symfony2 based permissions',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
+    
     {
         title:'BaseJS',
         url: '#',
         description:'This library provide basic VanillaJS functions',
         technology: 'Javascript',
         color: 'color-4',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Help Bundle',
-        url: '#',
-        description:'Add markdown files as bundle help',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Report Bundle',
-        url: '#',
-        description:'Basic bundle for managing/export reports.',
-        technology: 'Symfony2',
-        color: 'color-1',
         links:[
             {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
         ]
@@ -414,116 +146,6 @@ var inline_projects = [
         description:'A VanillaJS helper library for traversing DOM',
         technology: 'Javascript',
         color: 'color-4',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'AnimateJS',
-        url: '#',
-        description:'A VanillaJS library with CSS3 provide animation features. ',
-        technology: 'Javascript',
-        color: 'color-4',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'WidgetJS',
-        url: '#',
-        description:'Simple VanillaJS widget library.',
-        technology: 'Javascript',
-        color: 'color-4',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Front Bundle',
-        url: '#',
-        description:'This bundle help to build front page for websites',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Organization Bundle',
-        url: '#',
-        description:'A Symfony2 bundle for manage organizations.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'PhpDoc Bundle',
-        url: '#',
-        description:'Create static documentation files using Symfony2',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'FormJS',
-        url: '#',
-        description:'A VanillaJS library for handle HTML Forms',
-        technology: 'Javascript',
-        color: 'color-4',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Util Bundle',
-        url: '#',
-        description:'A Symfony2 bundle have basic functions.',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Migration Bundle',
-        url: '#',
-        description:'Data migration from any source',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'EmailBuilder Bundle',
-        url: '#',
-        description:'It is a long established fact that a reader will be distracted by the',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'Email Bundle',
-        url: '#',
-        description:'It is a long established fact that a reader will be distracted by the',
-        technology: 'Symfony2',
-        color: 'color-1',
-        links:[
-            {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
-        ]
-    },
-    {
-        title:'AngularForm Bundle',
-        url: '#',
-        description:'Dynamic forms using AngularJS',
-        technology: 'Symfony2',
-        color: 'color-1',
         links:[
             {label:'github',url:'http://www.domjs.com'},{label:'knpbundles',url:'http://knd/domjs'}
         ]
